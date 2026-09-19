4b70bc3f6470bd06e18b8b932b07e9ed2bd4685a
shivansh-kale <shivanshkale17@gmail.com>
2026-09-19 19:31:48 +0530

    first postman api testing done

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 const app = require("./src/app")
 
-app.get("/" , (req, res)=>{
-    res.send("hiii, fisrt node api here ! ")
+const express = require("express");
+app.use(express.json())
+
+const notes = []
+
+app.post("/notes", (req,res)=> {
+    console.log(req.body)
 })
 
-app.get("/note/:index" , ( req, res) => {
+app.get("/notes" , (req, res)=>{
+    res.status(200).json({
+        message :" Notes fetcched successfully",
+        notes : notes 
+    })
+})
+
+app.get("/notes/:index" , ( req, res) => {
     const name = req.params.index
     res.send(`hi , ${name}`)
+})
+
+
+
+app.delete("/notes", (req, res)=> {
+
+})
+
+app.patch("/notes" , (req, res)=> {
+
 })
 
 app.listen( 3000, ()=> {
     console.log("server is running on port 3000");
 })
+
+
